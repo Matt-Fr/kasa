af7db4143103a2dce0c4359468c8c9ee0c22448e
Matt-Fr <matthieu_franck@orange.fr>
2023-06-30 19:05:43 +0200

    footer done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
+import logoFooter from "../assets/logoFooter.svg";
 
 const Footer = () => {
-  return <footer className="text-white bg-black ">Footer</footer>;
+  return (
+    <footer className="flex flex-col items-center justify-center text-white bg-black h-52">
+      <Image
+        src={logoFooter}
+        alt="logo kasa footer"
+        className="items-center w-32 m-6 md:w-52"
+      ></Image>
+      <span className="text-xs md:text-2xl ">
+        © 2023 Kasa. All rights reserved
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
