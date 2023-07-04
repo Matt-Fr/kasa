d7c8938f96dfb2f2c7917497352711cbabd83491
Matt-Fr <matthieu_franck@orange.fr>
2023-07-04 14:56:27 +0200

    added animation chevron

--- app/components/Collapse.tsx
+++ app/components/Collapse.tsx
@@ -9,14 +9,25 @@ interface CollapseProps {
 }
 
 const Collapse: React.FC<CollapseProps> = ({ title, info }) => {
-  const [showInfo, setShowInfo] = useState<boolean>(true);
+  const [showInfo, setShowInfo] = useState<boolean>(false);
 
   return (
     <article className="mt-5 text-xs md:mt-7 md:text-2xl">
       <header className="flex items-center justify-between px-2 py-1 text-white rounded bg-red">
         <h4 className="">{title}</h4>
-        <button className="flex items-center justify-center w-5 h-full md:w-8">
-          <Image src={chevron} alt="icon chevron" className="w-full "></Image>
+        <button
+          className="flex items-center justify-center w-5 h-full md:w-8"
+          onClick={() => setShowInfo(!showInfo)}
+        >
+          <Image
+            src={chevron}
+            alt="icon chevron"
+            className={`transform ${
+              showInfo
+                ? "rotate-180 w-full transition-transform"
+                : "w-full transition-transform"
+            }`}
+          ></Image>
         </button>
       </header>
       <p>{info}</p>
