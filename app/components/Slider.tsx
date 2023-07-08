a00f7692cf96b12a3f9c6c4a957da185cb86a5c8
Matt-Fr <matthieu_franck@orange.fr>
2023-07-08 17:29:04 +0200

    added slider

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -1,22 +1,72 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-import bgAbout from "../assets/bgAbout.png";
+import React, { useEffect, useState } from "react";
 import chevron from "../assets/chevron.svg";
 
-const Slider = () => {
+type SliderProps = {
+  pictures: string[]; // An array of strings representing the image URLs
+};
+
+const Slider = ({ pictures }: SliderProps) => {
+  const images = pictures;
+  const [index, setIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const lastIndex = images.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, images]);
+
   return (
-    <div className="flex h-64 col-span-full md:h-[415px]  overflow-hidden rounded-2xl relative">
-      <Image src={bgAbout} alt="a pic" className="object-cover w-full "></Image>
+    <div className="flex h-64 col-span-full md:h-[415px] overflow-hidden  rounded-2xl relative">
+      {images.map((picture, picindex) => {
+        let position = " translate-x-full opacity-0";
+        if (picindex === index) {
+          position = " opacity-100";
+        }
+        if (
+          picindex === index - 1 ||
+          (index === 0 && picindex === images.length - 1)
+        ) {
+          position = "-translate-x-full opacity-0 -";
+        }
+        if (images.length === 1) {
+          position = "z-50 opacity-0";
+        }
+        return (
+          <Image
+            fill
+            src={picture}
+            alt=""
+            key={picture}
+            className={` object-cover transition-all duration-300 ease-linear ${position} `}
+          />
+        );
+      })}
       <Image
         src={chevron}
+        onClick={() => {
+          setIndex(index - 1);
+        }}
         alt="left arrow"
         className="absolute transform rotate-90 -translate-y-1/2 cursor-pointer md:w-12 left-4 top-1/2"
       ></Image>
       <Image
+        onClick={() => {
+          setIndex(index + 1);
+        }}
         src={chevron}
         alt="right arrow"
         className="absolute transform -rotate-90 -translate-y-1/2 cursor-pointer md:w-12 right-4 top-1/2"
       ></Image>
+
+      <span className="absolute z-[-10] md:z-10 text-white  bottom-3 left-1/2">{`${
+        index + 1
+      } / ${images.length}`}</span>
     </div>
   );
 };
--- app/rent/[rentId]/page.tsx
+++ app/rent/[rentId]/page.tsx
@@ -47,7 +47,7 @@ const Page = () => {
     return (
       <>
         <section className="grid grid-cols-1 gap-3 md:grid-cols-4 md:gap-8">
-          <Slider></Slider>
+          <Slider pictures={pictures}></Slider>
           <section className="md:col-span-3 ">
             <h2 className="text-lg md:text-4xl text-red">{title}</h2>
             <span className="text-sm text-red md:text-lg">{location}</span>
